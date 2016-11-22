5cab6209d666c44fee578ece25bec4ff0328fc9f
Anubhav Singh <anubhavs286@gmail.com>
2016-11-22 06:55:40 +0000

    Created index and detail view

--- task/blog/views.py
+++ task/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Content, Content_Types
 
 # Create your views here.
+def index(request):
+    queryset=Content.objects.all()
+    context={
+        "text":"Posts",
+        "objects":queryset,
+    }
+    
+    return render(request, "index.html", context)
+    
+    
+def detail(request, id):
+    instance =get_object_or_404(Content, id=id)
+    
+    #content=get_object_or_404(Contents_Type, id=id)
+    object_list=instance.content_types_set.all()
+    context={
+        "title":instance.title,
+        "instance":instance,
+        
+        "object_list":object_list,
+        
+        
+    }
+    return render(request, "detail.html", context)
